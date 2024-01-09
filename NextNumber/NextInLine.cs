59990e24f93f3d0997acac56457f3041801c9dad
nate2293 <Mc_Garvey-N@ulster.ac.uk>
2024-01-09 20:42:18 +0000

    Add project files.

---
+++ NextNumber/NextInLine.cs
@@ -0,0 +1,26 @@
+﻿namespace NextNumber
+{
+    internal class NextInLine
+    {
+        public static int NextNum(int a)        // Expects and outputs interger
+        {
+            return a + 1;       // Returns num + 1
+        }
+
+        static void Main()
+        {
+            int resultOne = NextNum(5);     // Result - 6 
+            Console.WriteLine(resultOne);
+            Console.WriteLine();
+
+            int resultTwo = NextNum(9);     // Result - 10 
+            Console.WriteLine(resultTwo);
+            Console.WriteLine();
+
+            int resultThree = NextNum(30);     // Result - 31 
+            Console.WriteLine(resultThree);
+            Console.WriteLine();
+
+        }
+    }
+}
